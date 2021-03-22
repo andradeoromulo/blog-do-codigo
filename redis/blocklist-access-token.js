45b9646e93f0f821a6a0544ee74648b4afd224b5
Romulo Andrade <andrade.o.romulo@gmail.com>
2021-03-22 09:32:29 -0300

    Refatora blocklist de access tokens

--- redis/blocklist-access-token.js
+++ redis/blocklist-access-token.js
@@ -1,10 +1,10 @@
-const blocklist = require("./blocklist");
-const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const { createHash } = require("crypto");
+const redis = require("redis");
+const manipulaLista = require("./manipula-lista");
 
-const setAsync = promisify(blocklist.set).bind(blocklist);
-const existsAsync = promisify(blocklist.exists).bind(blocklist);
+const blocklist = redis.createClient({ prefix: "blocklist-access-token" });
+const manipulaBlocklist = manipulaLista(blocklist);
 
 function geraTokenHash(token) {
     return createHash("sha256").update(token).digest("hex");
@@ -14,12 +14,10 @@ module.exports = {
     adiciona: async token => {
         const dataExpiracao = jwt.decode(token).exp;
         const tokenHash = geraTokenHash(token);
-        await setAsync(tokenHash, "");
-        blocklist.expireat(tokenHash, dataExpiracao);
+        await manipulaBlocklist.adiciona(tokenHash, "", dataExpiracao);
     },
     contemToken: async token => {
         const tokenHash = geraTokenHash(token);
-        const resultado = await existsAsync(tokenHash);
-        return resultado === 1;
+        return await manipulaBlocklist.contemChave(tokenHash);
     }
 }
